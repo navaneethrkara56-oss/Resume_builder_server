44c8af025b62c25fb8767d0b4d1c7955e655844a
navaneethrkara56-oss <navaneethrkara56@gmail.com>
2026-09-09 09:31:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//import json server
+const jsonserver=require('json-server');
+//create server
+const server=jsonserver.create();
+//set path for/route for middleware
+const route=jsonserver.router('db.json');
+//create middlewares
+const middlewares=jsonserver.defaults();
+server.use(route);
+server.use(middlewares);
+//set port
+const PORT=3000;
+
+server.listen(PORT,()=>{
+    console,log('server started')
+})
